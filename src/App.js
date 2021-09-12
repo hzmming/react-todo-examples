6699c617d115cd689688fdc0880c51dea846910f
黄振明 <hzm@meitu.com>
2021-09-12 23:01:42 +0800

    feat: url参数可选匹配默认路径

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         注意此处children传的是<TodoList />，如果传成TodoList，会被当成普通函数执行，而不是创建React Element
         最直接的表现就是，TodoList里面用了hook立马报错。
         */}
-        <Route path="/:filter" children={<TodoList />}></Route>
+        <Route path="/:filter?" children={<TodoList />}></Route>
       </div>
       <Footer />
     </Router>
