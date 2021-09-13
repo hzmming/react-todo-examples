0d2d55cffa8782c38353ae97608a9a6ac8188ffd
黄振明 <hzm@meitu.com>
2021-09-13 19:54:39 +0800

    feat: 完善

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="info">
       <p>Double-click to edit a todo</p>
       <p>
-        Created by <a href="http://github.com/jacob-ebey/">jacob-ebey</a>
+        Created by <a href="https://github.com/hzmming/">hzmming</a>
       </p>
       <p>
         Part of <a href="http://todomvc.com">TodoMVC</a>
